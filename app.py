9db0f85659abd8d3a6f8e4fdb7e822ee505365ce
Adam Prisenznak <adam.prisen@gmail.com>
2020-05-14 20:06:49 +0200

    Created app

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+
+from security import authenticate, identity as identity_function
+from flask import jsonify
+from resources.user import UserRegister
+from resources.item import Item, ItemList
+from resources.store import Store, SotreList
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'sece12'
+api = Api(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+app.config['JWT_AUTH_URL_RULE'] = '/login'
+jwt = JWT(app, authenticate, identity_function)
+
+@jwt.auth_response_handler
+def customized_response_handler(access_token, identity):
+    return jsonify({
+        'access_token': access_token.decode('utf-8'),'user_id': identity.id
+    })
+
+
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
+api.add_resource(SotreList, '/stores')
+
+
+
+if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
+    app.run(port=5000, debug=True)
